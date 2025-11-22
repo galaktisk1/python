48138e31b7b2964cc4c78ef738b631bc4f6d2cba
galaktisk1 <calebjackson565@gmail.com>
2025-11-22 12:33:23 -0600

    test_television.py to test the Television class functionality.

--- test_television.py
+++ test_television.py
@@ -3,15 +3,12 @@ from television import Television
 
 
 class TestTelevision:
-    """Test suite for the Television class."""
-    
     def test_init(self):
         """Test that a new Television initializes with correct default values."""
         tv = Television()
         assert str(tv) == "Power = False, Channel = 0, Volume = 0"
     
     def test_power(self):
-        """Test that power() toggles the television on and off."""
         tv = Television()
         tv.power()
         assert "Power = True" in str(tv)
@@ -19,7 +16,6 @@ class TestTelevision:
         assert "Power = False" in str(tv)
     
     def test_mute(self):
-        """Test that mute() toggles mute status when TV is on."""
         tv = Television()
         tv.power()
         tv.volume_up()
@@ -29,14 +25,12 @@ class TestTelevision:
         assert "Volume = 1" in str(tv)
     
     def test_channel_up(self):
-        """Test that channel_up() increases the channel when TV is on."""
         tv = Television()
         tv.power()
         tv.channel_up()
         assert "Channel = 1" in str(tv)
     
     def test_channel_down(self):
-        """Test that channel_down() decreases the channel when TV is on."""
         tv = Television()
         tv.power()
         tv.channel_up()
@@ -44,14 +38,12 @@ class TestTelevision:
         assert "Channel = 0" in str(tv)
     
     def test_volume_up(self):
-        """Test that volume_up() increases the volume when TV is on."""
         tv = Television()
         tv.power()
         tv.volume_up()
         assert "Volume = 1" in str(tv)
     
     def test_volume_down(self):
-        """Test that volume_down() decreases the volume when TV is on."""
         tv = Television()
         tv.power()
         tv.volume_up()
